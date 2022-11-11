4d2c412f026505d2319c93722ec1a45c00d0c839
Ueeayo <SimoneCattaneo05@gmail.com>
2022-11-11 16:15:55 +0100

    Get in touch button

--- app.js
+++ app.js
@@ -6,27 +6,78 @@ const s2 = document.querySelector(".s2");
 const s3 = document.querySelector(".s3");
 const s4 = document.querySelector(".s4");
 const s5 = document.querySelector(".s5");
-const ctTitle = document.querySelector('.ct-title');
-//const mailCopy = document.querySelector('.copy');
-//const send = document.querySelector('.send-wrapper');
+const contactWrapper= document.querySelector('.contact-wrapper');
+const contact = document.querySelector('.contact')
+const paperPlaneWrapper = document.querySelector('.paper-plane');
+const paperPlane = document.querySelector("#plane");
+const contactTextWrapper = document.querySelector(".contact-txt")
+const contactText = document.querySelector(".change-txt")
+const ctTitle = document.querySelector('.ct-title')
+const cntAboveLine =  document.querySelector('.cnt-above-line');
+const cntAboveText =  document.querySelector('.cnt-above-txt');
+const cntUnderText =  document.querySelector('.cnt-under-txt');
 
+console.log("larghezza", window.innerWidth)
+console.log("altezza", window.innerHeight)
+console.log(contactText)
+//MENU
 menuSquare.addEventListener("mouseover",(e)=>{
-    console.log(e)
     menuText.style.opacity =1;
     menuText.style.transition ="all 1s ease-in-out";
 });
 
 menuSquare.addEventListener("mouseout",(e)=>{
-    console.log(e)
     menuText.style.transition ="";
     menuText.style.opacity =0;
     menuText.style.transition ="all 0.3s ease-in-out";
 });
+//CONTACT
+document.addEventListener("mouseover", (e) =>{
+if(e.target == contactTextWrapper || e.target == paperPlaneWrapper){
+    contactWrapper.style.backgroundPosition= "left";
+    contactText.textContent ="SimoneCattaneo05@gmail.com"
+    contactText.style.letterSpacing="0px";
+    contactText.style.fontSize="1.9rem"
+    contactText.style.fontWeight="300";
+    contactText.style.color="var(--main-white)";
+    contact.style.border ="none";
+    paperPlaneWrapper.style.borderRight ="1px solid transparent";
+    paperPlane.style.rotate ="45deg";
+    paperPlane.style.pointerEvent ="auto";
+    contactWrapper.style.filter="drop-shadow(-4px 4px 0px rgba(4, 43, 102, 0.473))";
+    
+    }
+    if(e.target == paperPlaneWrapper){
+        cntAboveLine.style.animation = "cnt-above 0.8s cubic-bezier(0.645, 0.045, 0.355, 1)"
+        cntAboveLine.style.animationFillMode = "forwards";
+        cntAboveText.style.animation = "cnt-above-txt 1s cubic-bezier(0.645, 0.045, 0.355, 1)"
+        cntAboveText.style.animationFillMode = "forwards";
+    }
+})
+document.addEventListener("mouseout", (e) =>{
+    if(e.target == contactTextWrapper || e.target == paperPlaneWrapper){
+    contactWrapper.style.backgroundPosition= "right";
+    contactText.textContent ="Get in touch"
+    contactText.style.fontSize="2.8rem"
+    contactText.style.fontWeight="800";
+    contactText.style.color="var(--main-second)";
+    contactText.style.letterSpacing="2px";
+    contact.style.border ="1px solid var(--main-white)";
+    paperPlaneWrapper.style.borderRight ="1px solid var(--main-white)"; 
+    paperPlane.style.rotate ="0deg";
+    contactWrapper.style.filter="none";
+    cntAboveLine.style.animation = "none"
+    cntAboveLine.style.animation = "cnt-above-out 0.8s cubic-bezier(0.645, 0.045, 0.355, 1)"
+    cntAboveLine.style.animationFillMode = "forwards";
+    cntAboveText.style.animation = "none"
+    }
+})
 
+//NAV INDICATOR
 s1.classList.add("square-active")
 window.onscroll = function (event) {
     let scroll = window.pageYOffset;
-    console.log(scroll)
+    //console.log(scroll)
     if (scroll < 350) {
         squareIndicator.forEach(square =>{
             square.style.border = '1px solid var(--main-white)';
@@ -46,14 +97,20 @@ window.onscroll = function (event) {
     }
     }
     
-    mailCopy.addEventListener('click', ()=>{
-        navigator.clipboard.writeText("SimoneCattaneo05@gmail.com").then(() => {
-            console.log('Content copied to clipboard');
-            /* Resolved - text copied to clipboard successfully */
-          },() => {
-            console.error('Failed to copy');
-            /* Rejected - text failed to copy to the clipboard */
-          });
-        send.style.animation ="send 1.5s ease-in-out";
+    //COPY CONTACT
+    paperPlaneWrapper.addEventListener('click', (e)=>{
+    navigator.clipboard.writeText("SimoneCattaneo05@gmail.com").then(() => {
+    console.log('Content copied to clipboard');
+    /* Resolved - text copied to clipboard successfully */
+    },() => {
+    console.error('Failed to copy');
+    /* Rejected - text failed to copy to the clipboard */
+    });
+    e.preventDefault;
+    cntUnderText.classList.remove("cnt-under-active");
+    void cntUnderText.offsetWidth;
 
-    })
+    cntUnderText.classList.add("cnt-under-active")
+    
+
+   });
